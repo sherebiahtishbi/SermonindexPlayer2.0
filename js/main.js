c6d328efba4eb70b70f9b021a6c5188df690ff7d
sherebiahtishbi@gmail.com <sherebiahtishbi@gmail.com>
2020-05-11 17:22:03 -0400

    Windows installer fixes

--- js/main.js
+++ js/main.js
@@ -5,7 +5,15 @@ Goal        : Main js for electron app for sermon downloader
 Change Log  : None
 */
 
+ //handle setupevents as quickly as possible
+ const setupEvents = require('../js/setupEvents');
+ if (setupEvents.handleSquirrelEvent()) {
+    // squirrel event handled and app will exit in 1000ms, so don't do anything else
+    return;
+ }
+
 const { app, BrowserWindow, screen, ipcMain, dialog } = require('electron');
+var path = require('path');
 var window;
 
 //create main GUI window
